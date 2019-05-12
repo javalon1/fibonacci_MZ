7a01e5b496bae5a92221b9a668a5ed63e29d0104
Mateusz Zagrzejewski <mateusz.zagrzejewski@gmail.com>
2019-05-12 11:38:09 +0100

    kolejne zmiany

--- src/main/java/com/sda/algorytymy/App.java
+++ src/main/java/com/sda/algorytymy/App.java
@@ -1,27 +1,53 @@
 package com.sda.algorytymy;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
- *
  */
 public class App {
 
-    public static int fibonacci (int n) {
-        if (n<=2) {
+    public static int fibonacci(int n) {
+        if (n <= 2) {
             return 1;
-        }else  {
-            return fibonacci (n-2) + fibonacci(n-1);
+        } else {
+            return fibonacci(n - 2) + fibonacci(n - 1);
         }
     }
 
-    public static void main(String[] args)
-    {
+    public static void fibonacci2() {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+
+        if (N < 2) {
+            System.out.println(N);
+        } else {
+            int A = 0;
+            int B = 1;
+            int C = 1;
+            int K = 2;
+
+            while (K < N) {
+                A = B;
+                B = C;
+                C = A + B;
+                K++;
+            }
+            System.out.println(C);
+
+        }
+    }
+
+    public static void main(String[] args) {
+
+
+        fibonacci2();
         System.out.println(fibonacci(5));
         int tab[] = new int[20];
-        tab [0] =0;
-        tab [1]=1;
+        tab[0] = 0;
+        tab[1] = 1;
         for (int i = 2; i < tab.length; i++) {
-            tab [i] = tab [i-1] + tab [i-2];
+            tab[i] = tab[i - 1] + tab[i - 2];
 
             System.out.println(tab[i]);
 
