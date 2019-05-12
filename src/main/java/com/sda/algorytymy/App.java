280f276c15d3f94ad20c39cc713b1a268df7d644
Mateusz Zagrzejewski <mateusz.zagrzejewski@gmail.com>
2019-05-12 10:04:50 +0100

    zmiany

--- src/main/java/com/sda/algorytymy/App.java
+++ src/main/java/com/sda/algorytymy/App.java
@@ -4,10 +4,19 @@ package com.sda.algorytymy;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    public static void main(String[] args) {
+        int tab[] = new int[20];
+        tab [0] =0;
+        tab [1]=1;
+        for (int i = 2; i < tab.length; i++) {
+            tab [i] = tab [i-1] + tab [i-2];
+
+            System.out.println(tab[i]);
+
+
+        }
+
     }
 }
